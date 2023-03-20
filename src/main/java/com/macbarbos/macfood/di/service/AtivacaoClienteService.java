d1145532523715d5e3ccb0df8149c8cb18bf97f4
Marcos Alexandre Barbosa <macbarbos@gmail.com>
2023-03-20 19:42:08 -0300

    Pontos de injeção com @Autowired.

--- src/main/java/com/macbarbos/macfood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/macbarbos/macfood/di/service/AtivacaoClienteService.java
@@ -1,22 +1,39 @@
 package com.macbarbos.macfood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.macbarbos.macfood.di.modelo.Cliente;
 import com.macbarbos.macfood.di.notificacao.Notificador;
 
+@Component
 public class AtivacaoClienteService {
 
+	@Autowired(required = false)
 	private Notificador notificador;
 	
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-		
-		System.out.println("AtivacaoClienteService: " + notificador);
-	}
+//	@Autowired
+//	public AtivacaoClienteService(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+//	
+//	public AtivacaoClienteService(String qualquer) {
+//		
+//	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		if (notificador != null) {
+			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		} else {
+			System.out.println("Não existe notificador, mas cliente foi ativado");
+		}
 	}
+
+//	@Autowired
+//	public void setNotificador(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
 	
 }
