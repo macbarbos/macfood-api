e6abff40e47f2f467e1b0d8ccad34e0d8c072ed8
Marcos Alexandre Barbosa <macbarbos@gmail.com>
2023-03-21 17:32:20 -0300

    Acessando propriedades com @ConfigurationProperties.

--- src/main/java/com/macbarbos/macfood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/macbarbos/macfood/di/notificacao/NotificadorEmail.java
@@ -1,24 +1,24 @@
 package com.macbarbos.macfood.di.notificacao;
 
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.macbarbos.macfood.di.modelo.Cliente;
 
-@Profile("prod")
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
 	
-	public NotificadorEmail() {
-		System.out.println("NotificadorEmail REAL");
-	}
+	@Autowired
+	private NotificadorProperties properties;
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		System.out.println("Host: " + properties.getHostServidor());
+		System.out.println("Porta: " + properties.getPortaServidor());
+		
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
 }
-
