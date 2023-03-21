1caf3df3f24329eb61f3556bc40c7425456a4c4d
Marcos Alexandre Barbosa <macbarbos@gmail.com>
2023-03-21 12:23:10 -0300

    Criando métodos de callback do ciclo de vida dos beans.

--- src/main/java/com/macbarbos/macfood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/macbarbos/macfood/di/service/AtivacaoClienteService.java
@@ -1,25 +1,33 @@
 package com.macbarbos.macfood.di.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import com.macbarbos.macfood.di.modelo.Cliente;
 import com.macbarbos.macfood.di.notificacao.NivelUrgencia;
 import com.macbarbos.macfood.di.notificacao.Notificador;
 import com.macbarbos.macfood.di.notificacao.TipoDoNotificador;
 
-@Component
-public class AtivacaoClienteService {
+//@Component
+public class AtivacaoClienteService /* implements InitializingBean, DisposableBean */{
 
 	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 	@Autowired
 	private Notificador notificador;
+	
+	//@PostConstruct
+	public void init() {
+		System.out.println("INIT" + notificador);
+	}
+	//@PreDestroy
+	public void destroy() {
+		System.out.println("DESTROY");
+	}
 	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
-
+	
 }
 	
