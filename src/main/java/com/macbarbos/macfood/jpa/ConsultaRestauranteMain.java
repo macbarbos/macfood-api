be2b2883364a591dca9dfa72bf242915df879f77
Marcos Alexandre Barbosa <macbarbos@gmail.com>
2023-03-24 16:28:11 -0300

    Usando @ManyToMOne e @ManyToOne

--- src/main/java/com/macbarbos/macfood/jpa/ConsultaRestauranteMain.java
+++ src/main/java/com/macbarbos/macfood/jpa/ConsultaRestauranteMain.java
@@ -13,18 +13,18 @@ import com.macbarbos.macfood.domain.repository.RestauranteRepository;
 public class ConsultaRestauranteMain {
 
 	public static void main(String[] args) {
-		
 		ApplicationContext applicationContext = new SpringApplicationBuilder(MacfoodApiApplication.class)
 				.web(WebApplicationType.NONE)
 				.run(args);
-
+		
 		RestauranteRepository restauranteRepository = applicationContext.getBean(RestauranteRepository.class);
 		
-		List<Restaurante> restaurantes = restauranteRepository.listar();
+		List<Restaurante> todosRestaurantes = restauranteRepository.listar();
 		
-		for(Restaurante restaurante : restaurantes) {
-			System.out.println(restaurante.getNome());
+		for (Restaurante restaurante : todosRestaurantes) {
+			System.out.printf("%s - %f - %s\n", restaurante.getNome(),
+					restaurante.getTaxaFrete(), restaurante.getCozinha().getNome());
 		}
 	}
-
+	
 }
