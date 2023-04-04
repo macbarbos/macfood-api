1d614221aed6eadde0fe23893a5b06a3674b84e2
Marcos Alexandre Barbosa <macbarbos@gmail.com>
2023-04-04 11:16:08 -0300

    Corrigindo o Status HTTP para um resource inexistente.

--- src/main/java/com/macbarbos/macfood/api/controller/CozinhaController.java
+++ src/main/java/com/macbarbos/macfood/api/controller/CozinhaController.java
@@ -3,8 +3,6 @@ package com.macbarbos.macfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,17 +28,13 @@ public class CozinhaController {
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
 		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
 		
-//		podemos definir vários retornos com if's		
-//		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-//		return ResponseEntity.ok(cozinha);
+		if(cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
 		
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.LOCATION, "http://api.macfood.local:8080/cozinhas");
+//		return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); ou
+		return ResponseEntity.notFound().build();
 		
-		return ResponseEntity
-				.status(HttpStatus.FOUND)
-				.headers(headers)
-				.build();
 	}
 	
 }
