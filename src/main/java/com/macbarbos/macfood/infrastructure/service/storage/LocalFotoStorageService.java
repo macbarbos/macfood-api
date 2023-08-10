bae42863cbf3b100ccd7e0dbfcbc1df1c2c6111f
Marcos Alexandre Barbosa <macbarbos@gmail.com>
2023-08-10 17:21:56 -0300

    Implementando o serviço storage da AWS na Amazon S3 (Criando chaves de acesso para API da AWS, criando o bean de configuração, adicionando o SDK Java da Amazon S3 no projeto e configurando as credenciais).

--- src/main/java/com/macbarbos/macfood/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/macbarbos/macfood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -5,17 +5,18 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
+import com.macbarbos.macfood.core.storage.StorageProperties;
 import com.macbarbos.macfood.domain.service.FotoStorageService;
 
-@Service
+//@Service
 public class LocalFotoStorageService implements FotoStorageService {
 
-	@Value("${macfood.storage.local.diretorio-fotos}")
-	private Path diretorioFotos;
+	@Autowired
+	private StorageProperties localFotoStorageService;
 	
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
@@ -30,7 +31,7 @@ public class LocalFotoStorageService implements FotoStorageService {
 	}
 	
 	private Path getArquivoPath(String nomeArquivo) {
-		return diretorioFotos.resolve(Path.of(nomeArquivo));
+		return localFotoStorageService.getLocal().getDiretorioFotos().resolve(Path.of(nomeArquivo));
 	}
 
 	@Override
