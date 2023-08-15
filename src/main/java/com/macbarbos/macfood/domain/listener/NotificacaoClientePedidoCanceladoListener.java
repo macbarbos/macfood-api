f95322a3f58e4ca029153b0021371f89a5340be8
Marcos Alexandre Barbosa <macbarbos@gmail.com>
2023-08-15 17:52:33 -0300

    Reagindo a Domain Events em fases especificas da transação e implementando novo evento para envio de email ao cancelar um pedido.

--- src/main/java/com/macbarbos/macfood/domain/listener/NotificacaoClientePedidoCanceladoListener.java
+++ src/main/java/com/macbarbos/macfood/domain/listener/NotificacaoClientePedidoCanceladoListener.java
@@ -2,33 +2,31 @@ package com.macbarbos.macfood.domain.listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-import com.macbarbos.macfood.domain.event.PedidoConfirmadoEvent;
+import com.macbarbos.macfood.domain.event.PedidoCanceladoEvent;
 import com.macbarbos.macfood.domain.model.Pedido;
 import com.macbarbos.macfood.domain.service.EnvioEmailService;
 import com.macbarbos.macfood.domain.service.EnvioEmailService.Mensagem;
 
 @Component
-public class NotificacaoClientePedidoConfirmadoListener {
+public class NotificacaoClientePedidoCanceladoListener {
 	
 	@Autowired
 	private EnvioEmailService envioEmail;
-
-	//Por padrão o @TransactionalEventListener já vem como AFTER_COMMIT
-	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-	public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
+	
+	@TransactionalEventListener
+	public void aoCancelarPedido(PedidoCanceladoEvent event) {
 		Pedido pedido = event.getPedido();
 		
-		var mensagen = Mensagem.builder()
-				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado.")
-				.corpo("pedido-confirmado.html")
+		var mensagem = Mensagem.builder()
+				.assunto(pedido.getRestaurante().getNome() + " - Pedido Cancelado")
+				.corpo("pedido-cancelado.html")
 				.variavel("pedido", pedido)
 				.destinatario(pedido.getCliente().getEmail())
 				.build();
-
-		envioEmail.enviar(mensagen);
+		
+		envioEmail.enviar(mensagem);
 	}
 
 }
