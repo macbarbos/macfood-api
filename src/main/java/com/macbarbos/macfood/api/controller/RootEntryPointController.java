5d20caf498ea132afa2889aa90e1323d697497f2
Marcos Alexandre Barbosa <macbarbos@gmail.com>
2023-09-13 13:34:33 -0300

    Implementando o Root Entry Point da API.

---
+++ src/main/java/com/macbarbos/macfood/api/controller/RootEntryPointController.java
@@ -0,0 +1,42 @@
+package com.macbarbos.macfood.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.macbarbos.macfood.api.MacFoodLinks;
+
+import springfox.documentation.annotations.ApiIgnore;
+
+@ApiIgnore
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class RootEntryPointController {
+
+	@Autowired
+	private MacFoodLinks macFoodLinks;
+	
+	@GetMapping
+	public RootEntryPointModel root() {
+		var rootEntryPointModel = new RootEntryPointModel();
+		
+		rootEntryPointModel.add(macFoodLinks.linkToCozinhas("cozinhas"));
+		rootEntryPointModel.add(macFoodLinks.linkToPedidos("pedidos"));
+		rootEntryPointModel.add(macFoodLinks.linkToRestaurantes("restaurantes"));
+		rootEntryPointModel.add(macFoodLinks.linkToGrupos("grupos"));
+		rootEntryPointModel.add(macFoodLinks.linkToUsuarios("usuarios"));
+		rootEntryPointModel.add(macFoodLinks.linkToPermissoes("permissoes"));
+		rootEntryPointModel.add(macFoodLinks.linkToFormasPagamento("formas-pagamento"));
+		rootEntryPointModel.add(macFoodLinks.linkToEstados("estados"));
+		rootEntryPointModel.add(macFoodLinks.linkToCidades("cidades"));
+		
+		return rootEntryPointModel;
+	}
+	
+	private static class RootEntryPointModel extends RepresentationModel<RootEntryPointModel> {
+	}
+	
+}
